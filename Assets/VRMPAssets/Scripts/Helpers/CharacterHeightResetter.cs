ee61e86dae602ce4452dd0831bf223c095b1dd9f
Neil <bn0010100@gmail.com>
2025-01-31 10:10:31 -0600

    Fix "Shoot Up Into Sky" by reset height to 1.2m (#14)
    * Fix "Shoot Up Into Sky" by reset height to [1-2]m
    
    * Use a fixed reset height

---
+++ Assets/VRMPAssets/Scripts/Helpers/CharacterHeightResetter.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Teleportation;
+
+namespace XRMultiplayer
+{
+    public class CharacterHeightResetter : MonoBehaviour
+    {
+        [SerializeField] Vector2 m_MinMaxHeight = new Vector2(1.0f, 2.0f);
+        [SerializeField] float m_ResetHeight = 1.2f;
+        TeleportationProvider m_TeleportationProvider;
+
+        private void Start()
+        {
+            m_TeleportationProvider = GetComponentInChildren<TeleportationProvider>();
+        }
+
+        void Update()
+        {
+            float currentHeight = transform.position.y;
+            if (currentHeight < m_MinMaxHeight.x || currentHeight > m_MinMaxHeight.y)
+            {
+                ResetHeight();
+            }
+        }
+
+        void ResetHeight()
+        {
+            Vector3 newPosition = transform.position;  // Get current position
+            newPosition.y = m_ResetHeight;        // Only modify Y value (height)
+
+            TeleportRequest teleportRequest = new()
+            {
+                destinationPosition = newPosition,
+                destinationRotation = transform.rotation  // Keep current rotation
+            };
+
+            if (!m_TeleportationProvider.QueueTeleportRequest(teleportRequest))
+            {
+                Utils.LogWarning("Failed to queue teleport request");
+            }
+        }
+    }
+} 
