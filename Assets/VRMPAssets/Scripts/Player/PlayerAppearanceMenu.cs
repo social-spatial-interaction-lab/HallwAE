8b21e6cdb711b745f0980149cd277de12ab6e5ad
Satyam Rajput <satyam04rajput02@gmail.com>
2025-02-16 19:00:50 -0600

    Implement PlayerPrefs name persistence and update local name tag

--- Assets/VRMPAssets/Scripts/Player/PlayerAppearanceMenu.cs
+++ Assets/VRMPAssets/Scripts/Player/PlayerAppearanceMenu.cs
@@ -6,17 +6,29 @@ using UnityEngine.UI;
 namespace XRMultiplayer
 {
     /// <summary>
-    /// A simple example of how to setup a player appearance menu and utilize the bindable variables.
+    /// A simple example of how to setup a player appearance menu,
+    /// utilize the bindable variables, AND use PlayerPrefs for persistence.
     /// </summary>
     public class PlayerAppearanceMenu : MonoBehaviour
     {
+        [Header("Color Setup")]
         [SerializeField] Color[] m_PlayerColors;
-        [SerializeField] TMP_InputField m_PlayerNameInputField;
         [SerializeField] Image m_PlayerIconColor;
 
+        [Header("Name Field")]
+        [SerializeField] TMP_InputField m_PlayerNameInputField;
+
+        // PlayerPrefs keys
+        private const string USER_NAME_KEY = "UserName";
+        // Example key if an avatar dropdown is added later:
+        private const string AVATAR_INDEX_KEY = "AvatarIndex";
 
         void Awake()
         {
+            string savedName = PlayerPrefs.GetString(USER_NAME_KEY, "Player");
+
+            XRINetworkGameManager.LocalPlayerName.Value = savedName;
+
             XRINetworkGameManager.LocalPlayerName.Subscribe(SetPlayerName);
             XRINetworkGameManager.LocalPlayerColor.Subscribe(SetPlayerColor);
         }
@@ -34,12 +46,16 @@ namespace XRMultiplayer
         }
 
         /// <summary>
-        /// Use this to set the player's name so it triggers the bindable variable
+        /// Called by a Unity UI InputField (OnEndEdit) or a button that passes the field's text.
+        /// This sets the player's name network-wide AND saves it in PlayerPrefs.
         /// </summary>
-        /// <param name="text"></param>
         public void SubmitNewPlayerName(string text)
         {
+            
             XRINetworkGameManager.LocalPlayerName.Value = text;
+
+            PlayerPrefs.SetString(USER_NAME_KEY, text);
+            PlayerPrefs.Save();
         }
 
         /// <summary>
@@ -51,23 +67,33 @@ namespace XRMultiplayer
             List<Color> availableColors = new(m_PlayerColors);
             if (availableColors.Remove(XRINetworkGameManager.LocalPlayerColor.Value))
             {
-
-                XRINetworkGameManager.LocalPlayerColor.Value = availableColors[Random.Range(0, availableColors.Count)];
+                XRINetworkGameManager.LocalPlayerColor.Value 
+                 = availableColors[Random.Range(0, availableColors.Count)];
             }
             else
             {
-                XRINetworkGameManager.LocalPlayerColor.Value = m_PlayerColors[Random.Range(0, m_PlayerColors.Length)];
+                XRINetworkGameManager.LocalPlayerColor.Value 
+                    = m_PlayerColors[Random.Range(0, m_PlayerColors.Length)];
             }
         }
 
+        /// <summary>
+        /// Gets called automatically whenever LocalPlayerName changes.
+        /// Updates the TMP_InputField to show the current name.
+        /// </summary>
         void SetPlayerName(string newName)
         {
             m_PlayerNameInputField.text = newName;
         }
 
+        /// <summary>
+        /// Gets called automatically whenever LocalPlayerColor changes.
+        /// Updates the icon color in your UI.
+        /// </summary>
         void SetPlayerColor(Color color)
         {
             m_PlayerIconColor.color = color;
         }
     }
 }
+
