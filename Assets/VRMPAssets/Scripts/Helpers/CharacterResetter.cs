06b4aabdd33c0ea6d8e22c3a18ec8e3b47f308b8
neilbb <bn0010100@gmail.com>
2025-03-03 13:07:32 -0600

    Fix: Position out of range. 1. Always reset height to 0. 2. The player must be within a circle with a radius of 15 meters.

--- Assets/VRMPAssets/Scripts/Helpers/CharacterResetter.cs
+++ Assets/VRMPAssets/Scripts/Helpers/CharacterResetter.cs
@@ -5,7 +5,6 @@ namespace XRMultiplayer
 {
     public class CharacterResetter : MonoBehaviour
     {
-        [SerializeField] Vector2 m_MinMaxHeight = new Vector2(-2.5f, 25.0f);
         [SerializeField] float m_ResetDistance = 75.0f;
         [SerializeField] Vector3 offlinePosition = new Vector3(0, .5f, -12.0f);
         [SerializeField] Vector3 onlinePosition = new Vector3(0, .15f, 0);
@@ -15,7 +14,6 @@ namespace XRMultiplayer
         {
             XRINetworkGameManager.Connected.Subscribe(UpdateResetPosition);
             m_TeleportationProvider = GetComponentInChildren<TeleportationProvider>();
-
             m_ResetPosition = offlinePosition;
             ResetPlayer();
         }
@@ -36,14 +34,6 @@ namespace XRMultiplayer
         // Update is called once per frame
         void Update()
         {
-            if (transform.position.y < m_MinMaxHeight.x)
-            {
-                ResetPlayer();
-            }
-            else if (transform.position.y > m_MinMaxHeight.y)
-            {
-                ResetPlayer();
-            }
             if (Mathf.Abs(transform.position.x) > m_ResetDistance || Mathf.Abs(transform.position.z) > m_ResetDistance)
             {
                 ResetPlayer();
